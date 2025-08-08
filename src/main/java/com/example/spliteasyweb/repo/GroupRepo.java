d2cd21d4a78462c5ce76b9081eab29be2fe56151
cotibodereau <constanzabodereau@mi.unc.edu.ar>
2025-08-08 19:52:23 -0300

    Update GroupRepo.java

--- src/main/java/com/example/spliteasyweb/repo/GroupRepo.java
+++ src/main/java/com/example/spliteasyweb/repo/GroupRepo.java
@@ -2,9 +2,11 @@ package com.example.spliteasyweb.repo;
 
 import com.example.spliteasyweb.model.GroupEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface GroupRepo extends JpaRepository<GroupEntity, Long> {
-  Optional<GroupEntity> findBySlug(String slug);
-  boolean existsBySlug(String slug);
+    Optional<GroupEntity> findBySlug(String slug);
+    List<GroupEntity> findByOwnerSessionOrderByIdDesc(String ownerSession);
 }
